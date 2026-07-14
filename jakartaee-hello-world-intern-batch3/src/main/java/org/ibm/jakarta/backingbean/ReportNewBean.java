7a0e6676efdb280715510677f7b45fa5079120bb
Miracle0717 <gcburwell@up.edu.ph>
2026-07-14 15:40:21 +0800

    uploaded activity 1 for JAKARTAEE

---
+++ jakartaee-hello-world-intern-batch3/src/main/java/org/ibm/jakarta/backingbean/ReportNewBean.java
@@ -0,0 +1,45 @@
+package org.ibm.jakarta.backingbean;
+
+import org.ibm.jakarta.dto.ReportDto;
+import org.ibm.jakarta.infrastructure.repository.ReportRepository;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.validation.constraints.NotBlank;
+
+@Named
+@RequestScoped
+public class ReportNewBean {
+	@NotBlank(message = "You cannot leave the title blank.")
+	private String title;
+	private String detail;
+	
+	@Inject
+	private ReportRepository reportRepository;
+	
+	public String create() {
+		ReportDto newReport = new ReportDto();
+		newReport.setTitle(title);
+		newReport.setDetail(detail);
+		reportRepository.create(newReport);
+		
+		return "/reportList.xhtml?faces-redirect=true";
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getDetail() {
+		return detail;
+	}
+	
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+}
--- jakartaee-hello-world-intern-batch3/src/main/java/org/ibm/jakarta/infrastructure/repository/ReportRepository.java
+++ jakartaee-hello-world-intern-batch3/src/main/java/org/ibm/jakarta/infrastructure/repository/ReportRepository.java
@@ -1,5 +1,21 @@
 package org.ibm.jakarta.infrastructure.repository;
 
-public class ReportRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.ibm.jakarta.dto.ReportDto;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ReportRepository {
+	private List<ReportDto> reports = new ArrayList<>();
+	
+	public List<ReportDto> findAll() {
+		return reports;
+	}
+	
+	public void create(ReportDto report) {
+		reports.add(report);
+	}
 }
